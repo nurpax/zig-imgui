d949c3f09f626b98dd85a5f9ec0cfcc5d03b4706
Janne Hellsten <jjhellst@gmail.com>
2023-04-08 15:06:02 +0300

    sokol build

--- build.zig
+++ build.zig
@@ -24,6 +24,10 @@ pub fn build(b: *Builder) void {
         linkGlfw(exe, target);
         linkGlad(exe, target);
     }
+    {
+        const exe = exampleExe(b, "example_sokol_app", optimize, target);
+        linkSokol(exe, target);
+    }
 }
 
 fn exampleExe(b: *Builder, comptime name: []const u8, optimize: std.builtin.Mode, target: std.zig.CrossTarget) *LibExeObjStep {
@@ -32,6 +36,7 @@ fn exampleExe(b: *Builder, comptime name: []const u8, optimize: std.builtin.Mode
     exe.install();
 
     exe.addAnonymousModule("imgui", .{ .source_file = .{ .path = "zig-imgui/imgui.zig" } });
+    exe.addAnonymousModule("sokol", .{ .source_file = .{ .path = "sokol/sokol.zig" } });
 
     const run_step = b.step(name, "Run " ++ name);
     const run_cmd = exe.run();
@@ -62,5 +67,49 @@ fn linkVulkan(exe: *LibExeObjStep, target: std.zig.CrossTarget) void {
         exe.addObjectFile("examples/lib/win/vulkan-1.lib");
     } else {
         exe.linkSystemLibrary("vulkan");
+    }
+}
+
+fn linkSokol(exe: *LibExeObjStep, target: std.zig.CrossTarget) void {
+    _ = target;
+    const csources = [_][]const u8{
+        "sokol/c/sokol_app.c",
+        "sokol/c/sokol_gfx.c",
+        "sokol/c/sokol_time.c",
+        "sokol/c/sokol_audio.c",
+        "sokol/c/sokol_gl.c",
+        "sokol/c/sokol_debugtext.c",
+        "sokol/c/sokol_shape.c",
+        "sokol/c/sokol_log.c",
+    };
+    //const backend = if (lib.target.isDarwin()) .metal else if (lib.target.isWindows()) .d3d11 else .gl;
+    const backend = .d3d11;
+    std.debug.assert(exe.target.isWindows());
+    const backend_option = switch (backend) {
+        .d3d11 => "-DSOKOL_D3D11",
+        .metal => "-DSOKOL_METAL",
+        .gl => "-DSOKOL_GLCORE33",
+        .gles2 => "-DSOKOL_GLES2",
+        .gles3 => "-DSOKOL_GLES3",
+        .wgpu => "-DSOKOL_WGPU",
+        else => unreachable,
+    };
+
+    for (csources) |csrc| {
+        exe.addCSourceFile(csrc, &[_][]const u8{ "-DIMPL", backend_option });
+    }
+    if (exe.target.isLinux()) {
+        exe.linkSystemLibrary("X11");
+        exe.linkSystemLibrary("Xi");
+        exe.linkSystemLibrary("Xcursor");
+        exe.linkSystemLibrary("GL");
+        exe.linkSystemLibrary("asound");
+    } else if (exe.target.isWindows()) {
+        exe.linkSystemLibrary("kernel32");
+        exe.linkSystemLibrary("user32");
+        exe.linkSystemLibrary("gdi32");
+        exe.linkSystemLibrary("ole32");
+        exe.linkSystemLibrary("d3d11");
+        exe.linkSystemLibrary("dxgi");
     }
 }
