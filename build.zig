747403dd74c0247e447eef71f7434b3deaf1f00d
Janne Hellsten <jjhellst@gmail.com>
2024-01-11 23:38:20 +0200

    Use zig build modules

--- build.zig
+++ build.zig
@@ -1,10 +1,6 @@
 const std = @import("std");
 const builtin = @import("builtin");
 const path = std.fs.path;
-const Builder = std.build.Builder;
-const LibExeObjStep = std.build.LibExeObjStep;
-
-const imgui_build = @import("zig-imgui/imgui_build.zig");
 
 pub const Backend = enum {
     auto, // Windows: D3D11, macOS/iOS: Metal, otherwise: GL
@@ -16,96 +12,40 @@ pub const Backend = enum {
     wgpu,
 };
 
-// @src() is only allowed inside of a function, so we need this wrapper
-fn srcFile() []const u8 {
-    return @src().file;
-}
-const sep = std.fs.path.sep_str;
-const sokol_imgui_root_path = std.fs.path.dirname(srcFile()).?;
+pub fn build(b: *std.Build) void {
+    // Remove the default install and uninstall steps
+    b.top_level_steps = .{};
 
-pub fn build(b: *Builder) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    imgui_build.addTestStep(b, "test", optimize, target);
+    const sokol = b.addModule("sokol", .{ .root_source_file = .{ .path = "sokol/sokol.zig" } });
 
-    // {
-    //     const exe = exampleExe(b, "example_glfw_vulkan", optimize, target);
-    //     imgui_build.link(exe);
-    //     linkGlfw(exe, target);
-    //     linkVulkan(exe, target);
-    // }
-    // {
-    //     const exe = exampleExe(b, "example_glfw_opengl3", optimize, target);
-    //     imgui_build.link(exe);
-    //     linkGlfw(exe, target);
-    //     linkGlad(exe, target);
-    // }
-    {
-        const exe = exampleExe(b, "example_sokol_app", optimize, target);
-        linkSokolImgui(exe);
-    }
-}
+    const lib_opts = .{
+        .name = "sokol",
+        .target = target,
+        .optimize = optimize,
+    };
 
-fn exampleExe(b: *Builder, comptime name: []const u8, optimize: std.builtin.Mode, target: std.zig.CrossTarget) *LibExeObjStep {
-    const exe = b.addExecutable(.{ .name = name, .target = target, .optimize = optimize, .root_source_file = .{ .path = "examples/" ++ name ++ ".zig" } });
-    exe.install();
+    const lib = b.addStaticLibrary(lib_opts);
 
-    const run_step = b.step(name, "Run " ++ name);
-    const run_cmd = exe.run();
-    run_step.dependOn(&run_cmd.step);
-
-    return exe;
-}
-
-fn linkGlad(exe: *LibExeObjStep, target: std.zig.CrossTarget) void {
-    _ = target;
-    exe.addIncludePath("examples/include/c_include");
-    exe.addCSourceFile("examples/c_src/glad.c", &[_][]const u8{"-std=c99"});
-    //exe.linkSystemLibrary("opengl");
-}
-
-fn linkGlfw(exe: *LibExeObjStep, target: std.zig.CrossTarget) void {
-    if (target.isWindows()) {
-        exe.addObjectFile(if (target.getAbi() == .msvc) "examples/lib/win/glfw3.lib" else "examples/lib/win/libglfw3.a");
-        exe.linkSystemLibrary("gdi32");
-        exe.linkSystemLibrary("shell32");
-    } else {
-        exe.linkSystemLibrary("glfw");
-    }
-}
-
-fn linkVulkan(exe: *LibExeObjStep, target: std.zig.CrossTarget) void {
-    if (target.isWindows()) {
-        exe.addObjectFile("examples/lib/win/vulkan-1.lib");
-    } else {
-        exe.linkSystemLibrary("vulkan");
-    }
-}
-
-pub fn linkSokolImgui(exe: *LibExeObjStep) void {
-    const base = sokol_imgui_root_path ++ sep;
-    exe.addIncludePath(base ++ "zig-imgui");
-    exe.addIncludePath(base ++ "sokol");
-    exe.addIncludePath(base ++ "sokol/c");
-    exe.addAnonymousModule("sokol", .{ .source_file = .{
-        .path = base ++ "sokol/sokol.zig",
-    } });
-    imgui_build.link(exe);
+    lib.addIncludePath(.{ .path = "zig-imgui" });
+    lib.addIncludePath(.{ .path = "sokol" });
+    lib.addIncludePath(.{ .path = "sokol/c" });
 
     const csources = [_][]const u8{
-        base ++ "sokol/c/sokol_app.c",
-        base ++ "sokol/c/sokol_gfx.c",
-        base ++ "sokol/c/sokol_time.c",
-        base ++ "sokol/c/sokol_audio.c",
-        base ++ "sokol/c/sokol_gl.c",
-        base ++ "sokol/c/sokol_debugtext.c",
-        base ++ "sokol/c/sokol_shape.c",
-        base ++ "sokol/c/sokol_log.c",
-        base ++ "sokol/c/sokol_imgui.cpp",
-        base ++ "sokol/imgui_wrapper.c",
+        "sokol/c/sokol_app.c",
+        "sokol/c/sokol_gfx.c",
+        "sokol/c/sokol_time.c",
+        "sokol/c/sokol_audio.c",
+        "sokol/c/sokol_gl.c",
+        "sokol/c/sokol_debugtext.c",
+        "sokol/c/sokol_shape.c",
+        "sokol/c/sokol_log.c",
+        "sokol/c/sokol_imgui.cpp",
+        "sokol/imgui_wrapper.c",
     };
-    const backend: Backend = if (exe.target.isDarwin()) .metal else if (exe.target.isWindows()) .d3d11 else .gl;
+    const backend: Backend = if (target.result.os.tag == .macos) .metal else if (target.result.os.tag == .windows) .d3d11 else .gl;
     const backend_option = switch (backend) {
         .d3d11 => "-DSOKOL_D3D11",
         .metal => "-DSOKOL_METAL",
@@ -117,20 +57,37 @@ pub fn linkSokolImgui(exe: *LibExeObjStep) void {
     };
 
     inline for (csources) |csrc| {
-        exe.addCSourceFile(csrc, &[_][]const u8{ "-DIMPL", backend_option });
+        lib.addCSourceFile(.{ .file = .{ .path = csrc }, .flags = &[_][]const u8{ "-DIMPL", backend_option } });
     }
-    if (exe.target.isLinux()) {
-        exe.linkSystemLibrary("X11");
-        exe.linkSystemLibrary("Xi");
-        exe.linkSystemLibrary("Xcursor");
-        exe.linkSystemLibrary("GL");
-        exe.linkSystemLibrary("asound");
-    } else if (exe.target.isWindows()) {
-        exe.linkSystemLibrary("kernel32");
-        exe.linkSystemLibrary("user32");
-        exe.linkSystemLibrary("gdi32");
-        exe.linkSystemLibrary("ole32");
-        exe.linkSystemLibrary("d3d11");
-        exe.linkSystemLibrary("dxgi");
+    if (target.result.os.tag == .linux) {
+        lib.linkSystemLibrary("X11");
+        lib.linkSystemLibrary("Xi");
+        lib.linkSystemLibrary("Xcursor");
+        lib.linkSystemLibrary("GL");
+        lib.linkSystemLibrary("asound");
+    } else if (target.result.os.tag == .windows) {
+        lib.linkSystemLibrary("kernel32");
+        lib.linkSystemLibrary("user32");
+        lib.linkSystemLibrary("gdi32");
+        lib.linkSystemLibrary("ole32");
+        lib.linkSystemLibrary("d3d11");
+        lib.linkSystemLibrary("dxgi");
     }
+
+    // Add imgui files
+    lib.addCSourceFile(.{
+        .file = .{ .path = "zig-imgui/cimgui_unity.cpp" },
+        .flags = &[_][]const u8{
+            "-fno-sanitize=undefined",
+            "-ffunction-sections",
+        },
+    });
+    lib.linkLibCpp();
+
+    const imgui = b.addModule("imgui", .{ .root_source_file = .{ .path = "zig-imgui/imgui.zig" } });
+    imgui.linkLibrary(lib);
+    sokol.linkLibrary(lib);
+
+    // TODO not sure why this is necessary
+    sokol.addIncludePath(.{ .path = "sokol" });
 }
--- zig-imgui/tests.zig
+++ zig-imgui/tests.zig
@@ -7,15 +7,15 @@ extern fn igGET_FLT_MAX() callconv(.C) f32;
 extern fn igGET_FLT_MIN() callconv(.C) f32;
 
 test "FLT_MAX" {
-    assert(@bitCast(u32, ig.FLT_MAX) == @bitCast(u32, igGET_FLT_MAX()));
+    assert(@as(u32, @bitCast(ig.FLT_MAX)) == @as(u32, @bitCast(igGET_FLT_MAX())));
     const zig_flt_max: f32 = if (@hasDecl(std.math, "floatMax")) std.math.floatMax(f32) else std.math.f32_max;
-    assert(@bitCast(u32, ig.FLT_MAX) == @bitCast(u32, zig_flt_max));
+    assert(@as(u32, @bitCast(ig.FLT_MAX)) == @as(u32, @bitCast(zig_flt_max)));
 }
 
 test "FLT_MIN" {
-    assert(@bitCast(u32, ig.FLT_MIN) == @bitCast(u32, igGET_FLT_MIN()));
+    assert(@as(u32, @bitCast(ig.FLT_MIN)) == @as(u32, @bitCast(igGET_FLT_MIN())));
     const zig_flt_min: f32 = if (@hasDecl(std.math, "floatMin")) std.math.floatMin(f32) else std.math.f32_min;
-    assert(@bitCast(u32, ig.FLT_MIN) == @bitCast(u32, zig_flt_min));
+    assert(@as(u32, @bitCast(ig.FLT_MIN)) == @as(u32, @bitCast(zig_flt_min)));
 }
 
 test "Check version" {
@@ -44,7 +44,7 @@ fn compileEverything(comptime Outer: type, comptime skip_items: []const []const 
 test "Compile everything" {
     // This forces ig.FontAtlas to be analyzed before ig.Font,
     // which avoids a false positive circular dependency bug in stage 1.
-    var atlas: ig.FontAtlas = undefined;
+    const atlas: ig.FontAtlas = undefined;
     _ = atlas;
 
     @setEvalBranchQuota(10000);
@@ -54,8 +54,8 @@ test "Compile everything" {
     // Compile instantiations of Vector
     // The skipping logic doesn't work in 0.9.1 or earlier
     if (comptime builtin.zig_version.order(std.SemanticVersion.parse("0.9.1") catch unreachable) == .gt) {
-        const skip_value_type = &[_][]const u8{ "value_type" };
-        const skip_clear_delete = skip_value_type ++ &[_][]const u8{ "clear_delete" };
+        const skip_value_type = &[_][]const u8{"value_type"};
+        const skip_clear_delete = skip_value_type ++ &[_][]const u8{"clear_delete"};
         const skip_comparisons = skip_clear_delete ++ &[_][]const u8{ "contains", "find", "find_erase", "find_erase_unsorted", "eql" };
         compileEverything(ig.Vector(ig.Vec2), skip_clear_delete);
         compileEverything(ig.Vector(*ig.Vec4), skip_value_type);
